b137d9317308fe3fd5df5c435e4015b66c10af07
ShaikhJ18 <J.shk1980@gmail.com>
2023-10-18 20:17:05 -0400

    created essential files and folders for the project

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//import the icons
+import {B1HomeAlt, BitUser} from 'react-icons/bi'
+import {BsClipboardData, BsBriefcase, BsChatSquare} from 'react-icons/bs'
+//link
+import {Link} from 'react-scroll';
+const Nav = () => {
+  return <nav>
+    <div className="container mx-auto"></div>
+  </nav>;
+};
+
+export default Nav;
